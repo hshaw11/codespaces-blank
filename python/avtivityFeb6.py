50f394772da4e18851f2d71a2252ebcaada9ed70
hshaw11 <158070222+hshaw11@users.noreply.github.com>
2024-02-08 15:52:36 +0000

    Daily Work Feb 8

---
+++ python/avtivityFeb6.py
@@ -0,0 +1,5 @@
+# Q: What is complexity ?
+# A: The measure of how resource intensive a program can be.
+
+# Q: What is abstraction ?
+# A: abstraction is the process of simplifying or hiding complexity.
