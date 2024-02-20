bb8345c0aa0d0f906e1ca6e034bcf5c13a573bf6
hshaw11 <158070222+hshaw11@users.noreply.github.com>
2024-02-20 16:14:13 +0000

    Quiz

---
+++ HaleemQuiz.py
@@ -0,0 +1,61 @@
+
+#1. What built-in function would you use if you wanted
+# to print out data to a user?
+The built-in function I would use is print
+#2. What built-in function would you use if you wanted 
+# a user to pass in data to a program?
+The built-in function I would use is Input
+#3. Explain what complexity and abstraction are, then 
+# provide an example describing their relationship to one another.
+# You cannot use the following examples because they 
+# were shared in class; grocery/ electronics store, car, phone. 
+An abstraction is a simplified representation of something more complex and complexity is a field from computer science which analyzes algorithms based on the amount resources.
+#4. What does it mean when you encounter a syntax error?
+An error in the syntax of a sequence of characters that is supposed to be in a certain program.
+#5. What is a variable? Provide an example of a variable in code.
+A symbolic representations of any numbers.
+#6. Write a function in Python that when called will print out 
+# your name.
+print(haleem)
+#7. What is a function definition and a function call?
+Defining it names the function and specifies what to do when the function is called. Calling the function actually performs the specified actions with the indicated parameters.
+#8. Describe what each symbol means?
+
+# = // single equal
+# == // double equal
+# != // exclaimation equal
+
+#9. What will the output for the following code block be?
+var1 = 1
+var2 = 2
+var3 = "3"
+
+print(var1 + var2 + var3)
+
+
+#10. What will the output for the following code block be?
+listOne = [20, 40, 60, 80]
+listTwo = [20, 40, 60, 80]
+
+print(listOne == listTwo)
+
+# 11. Create the following python lists and include the following
+# data types for each list:
+# a- create a python list of strings. The strings should be items you
+# can find in a book bag.
+# b- Create a python list of floats. the floats should represent the 
+# price of each item in a book bag from the previous list.
+
+# 12. What are the Three (3) naming formats we use for naming variables, files, and functions?
+Camelcase, Pascalcase, and Snakecase
+# 13. Provide a coding scenario where you would you use a comparison operator in a program?
+
+# 14. Provide a coding scenario Where you would you use a logical operator in a program?
+
+# 15. Create a block of code where you would 
+# use a comparison operator with the follwing data:
+currentScore= 100
+highScore=2300
+
+# Bonus : What does the type() function do?
+It returns information about the data structures used to store data elements in a program.
--- activityFeb9.py
+++ activityFeb9.py
@@ -11,4 +11,4 @@ Q: what does the TypeError mean?
 A:
 
 Q: what does SyntaxError mean?
-A:
+A:a character or string incorrectly placed.
