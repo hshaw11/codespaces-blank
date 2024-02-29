3bcc1fac2f2053e2aa45fc58f055c5e9c106974e
hshaw11 <158070222+hshaw11@users.noreply.github.com>
2024-02-29 16:11:42 +0000

    warmup work

---
+++ warmup_Feb23.py
@@ -0,0 +1,8 @@
+Q: What are the two parts needed to create a function? Describe what they do.
+A: You need a Function Definintion and a Function Call
+
+Q: Explain the difference between a function parameter and a function argument.
+A: Parameter- goes inside the function definition
+
+Q:
+A:
---
+++ warmup_feb29.py
@@ -0,0 +1,4 @@
+# In your own words, describe what python conditional statements are (IF/ELSE); more specifically, try to describe how they work.
+'python conditional statements are constructs that allow a program to execute certain blocks of code based on whether a condition is true or flase'
+
+# Create a function that takes in user data and uses a conditional statement (if/else) to determine if a user has reached a games high score. Your function should ask the user what their score is. If the user score is above 3000, they have not
