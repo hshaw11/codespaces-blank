3e15741b883b2a9dea4e51864cf8c42b5d16f723
hshaw11 <158070222+hshaw11@users.noreply.github.com>
2024-03-07 16:20:45 +0000

    work 3/7/24

---
+++ classReviewNotes.py
@@ -0,0 +1,27 @@
+# PYTHON CLASS REVIEW
+
+# Vocab
+
+#1. Programs = Instructions for computers.
+
+#2. Programming Languages = Different ways we can write and
+# instructions to computers.
+
+# 3. Syntax = The formatting rules of a language.
+# In this case, a programming language.
+
+# additional note - when we are writing programs, all
+# we're doing is talking to the computer and giving
+# it instructions to complete a task.
+
+# String - Allows programmers to write isolated characters that won't impact the program; such as words.
+
+# Integer - A whole number; non-decimal number.
+
+# Float - A decimal number
+
+# Boolean - A true or false value.
+
+# Print() - A function that prints the value you provide to it.
+
+# Input() - A function that takes in that value you provide to it.
