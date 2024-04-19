17d38fa14b6a46e829db9ae6cbec55d0996a0177
hshaw11 <158070222+hshaw11@users.noreply.github.com>
2024-04-19 15:23:06 +0000

    April19Python.py

---
+++ PythonReview_April19.py
@@ -0,0 +1,47 @@
+#1A. Visit the web page link below and read the following article on asking questions:
+# https://www.betterup.com/blog/how-to-ask-good-questions
+
+# 1B. Based on what you've read, in your own words, describe what a good
+# question is. 
+
+#A good question is one that is clear, specific, and focused. A good question should be phrased in a way that will draw
+# out a meaningful and informative response.
+
+# 1C. In your opinion and in your own words, why do you think its
+# important to be able to ask good questions.
+
+# Asking good questions is a fundamental skill that strengthen effective learning and problem solving.
+
+# 2A. Visit the link web page below and complete the following learning style survey:
+# http://www.educationplanner.org/students/self-assessments/learning-styles-quiz.shtml
+
+#2B. Based on your completed survey results, which learning style are you? List, three (3)
+# things from the survey results that you feel best describe your learning style. 
+
+# The survey results say that I am a tactile learner and three things from the survey that best describe my learning style is
+# take frequent breaks, Do hands on activites, and Chew gum while studying.
+
+#2B. Based on your completed survey results, do you feel as though the result are arcurate in
+# reflecting your learning style? if yes, explain why, if no, explain why. Please write your
+# response in atleast three (3) complete sentences. 
+
+# I think the survey was partially right because some of the results were right but a lot of them was wrong.
+# I don't mind doing hands on activites but its not something I insist on doing.
+# And I like to work by myself more not in groups, group work is something that I dislike a lot  and if I can avoid it in any way shape or form I will.
+
+# 3A. Visit the  webp age link below and read the article on how to study:
+# https://summer.harvard.edu/blog/top-10-study-tips-to-study-like-a-harvard-student/
+
+# 3B. Which of the following tips do you feel you need to work on when studying? Explain
+# why in at least three (3) complete sentences.
+
+# One tip I need to work on is setting specific goals for my self when studying.
+# Another tip I need to work on is cultivate a productive space to work in because for some reason I always end up studying near people like my little brother who tends to be loud.
+# And the final tip I need to work on is to not cram I am always trying cram studying in right before I have a test and it never works for me.
+
+# 3C. Based on your learning style results, and the arcticle you just read on study tips,
+# think about how you can apply the study tips to your learning style. Then, in complete sentences
+# write down four (4) steps you could take to effectively study that is specific you you. 
+
+# If I added these things to my learning style my grades would be a lot better then what they are now.
+# 
