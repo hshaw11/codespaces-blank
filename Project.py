eba4a0ad4d5b472dbbba2be005cce37b4b6fcf71
hshaw11 <158070222+hshaw11@users.noreply.github.com>
2024-05-08 15:23:15 +0000

    project.py

---
+++ Project.py
@@ -0,0 +1,5 @@
+#You will use the next two weeks to design and develop a Python terminal program using all of the concepts you've learned throughout the semester.
+#our program should include more than 1 python function to accomplish it's task and should also use a combnitation of data types, operators, and constructs.
+#Your program should also include a python concept we haven't learned about in class (this can be somthing such as objects or built-in functions we did not discuss in class).
+#You are permitted to work individually or in groups.
+
