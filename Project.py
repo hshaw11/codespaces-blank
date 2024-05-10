907799025479bee1e9fe207b5de69f5ec1be636d
hshaw11 <158070222+hshaw11@users.noreply.github.com>
2024-05-10 14:45:03 +0000

    project

--- Project.py
+++ Project.py
@@ -13,3 +13,42 @@
 # a household budget, or a retiree looking to optimize your retirement savings, this app can provide the insights and tools you need to make informed financial decisions.
 # One of the key features of the Budget Tracker App is it's intuitive and straightforward interface. Users can easily navigate through the app's menu options, allowing
 # them to quickly add expenses, track their income, and view their overall budget.
+
+
+# great project draft -> just break this down into 3 pargraphs - 1 paragraph is 3-4 sentence
+
+# paragraph 1 - what is your idea?
+
+# paragraph 2 -  what problem are you trying to sovle?
+
+# paragraph 3 - who is your application designed for? 
+
+
+# Finance tracker app 
+# function 1 - add and withdraw money from account.
+# function 2 - send money to another user.
+
+
+# tools and context
+
+# we need 3 functions:
+# 1- add and withdraw money.
+# 2 - send money from account to another person.
+# 3 - a function that will hold the first 2. 
+
+# we need to have a function that will represent the banking app. this function will contain the add/ withdraw function and the send money function.
+# we will need a variable for the users account balance. This will be a float data type becuase it is a number. 
+# we will need to have an input function that will let users pick what banking option they want to use. example: please select an option, add funds, withdraw, and transfer
+
+# we need to create a function for adding money to account. 
+# inside of the deposit/ add funds function, we need to take a number that the user inputs and add it to the account variable. We will use the addition arithmatic operator.
+
+# we need to create a function for subtracting money from the account.
+# inside of the withdraw/ subtraction funds function, we need to take a number that the user input and subtract that number from our account variable. We will us the subtraction arithmatic operator. 
+
+# we need to create a funciton for removing and sending moneyt from the account. 
+
+def my_function():
+  print("Hello from a function")
+
+  my_function()
